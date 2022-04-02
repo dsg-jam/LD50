a3840cea971d6b9236a6cf39d1e9ca3cfe6d45b6
mineiwik <29404520+mineiwik@users.noreply.github.com>
2022-04-02 22:23:56 +0200

    Grid-based movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,22 @@
 extends Area2D
 
 var MAX_SPEED: int = 50
+var TILE_SIZE: int = 16
 
 func _ready():
-	pass
+	self.position = self.position.snapped(Vector2.ONE * TILE_SIZE)
+	self.position += Vector2.ONE * TILE_SIZE/2
 
+
+func _unhandled_input(event):
+	if event.is_action_pressed("move_up"):
+		self.position += Vector2.UP * TILE_SIZE
+	elif event.is_action_pressed("move_down"):
+		self.position += Vector2.DOWN * TILE_SIZE
+	elif event.is_action_pressed("move_left"):
+		self.position += Vector2.LEFT * TILE_SIZE
+	elif event.is_action_pressed("move_right"):
+		self.position += Vector2.RIGHT * TILE_SIZE
 
 func _process(delta):
-	if Input.is_action_pressed("move_up"):
-		self.position.y -= MAX_SPEED * delta
-	if Input.is_action_pressed("move_down"):
-		self.position.y += MAX_SPEED * delta
-	if Input.is_action_pressed("move_left"):
-		self.position.x -= MAX_SPEED * delta
-	if Input.is_action_pressed("move_right"):
-		self.position.x += MAX_SPEED * delta
+	pass
