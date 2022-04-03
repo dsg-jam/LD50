cb59fe0b989f15352a0e548d8fd56afceebbd714
mineiwik <29404520+mineiwik@users.noreply.github.com>
2022-04-03 11:32:58 +0200

    Add player collision, checkpoints and target point

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,18 @@ signal update_moves_left_counter(moves_left)
 
 var MAX_SPEED: int = 50
 var TILE_SIZE: int = 16
+
+var DIRECTIONS: Dictionary = {
+	"move_up": Vector2.UP,
+	"move_down": Vector2.DOWN,
+	"move_left": Vector2.LEFT,
+	"move_right": Vector2.RIGHT
+}
+
+onready var ray: RayCast2D = $RayCast2D
 export var MAX_PLAYER_MOVES: int  = 10
 var moves_left: int
+
 
 func _ready():
 	self.position = self.position.snapped(Vector2.ONE * TILE_SIZE)
@@ -15,27 +25,28 @@ func _ready():
 
 
 func _unhandled_input(event):
-	if moves_left <= 0 :
-		return
-	if event.is_action_pressed("move_up"):
-		self.position += Vector2.UP * TILE_SIZE
-		decreas_moves_left()
-	elif event.is_action_pressed("move_down"):
-		self.position += Vector2.DOWN * TILE_SIZE
-		decreas_moves_left()
-	elif event.is_action_pressed("move_left"):
-		self.position += Vector2.LEFT * TILE_SIZE
-		decreas_moves_left()
-	elif event.is_action_pressed("move_right"):
-		self.position += Vector2.RIGHT * TILE_SIZE
-		decreas_moves_left()
-	self.position.x = clamp(self.position.x, TILE_SIZE/2.0, 1048)
-	self.position.y = clamp(self.position.y, TILE_SIZE/2.0, 620)
+	for dir in DIRECTIONS:
+		if event.is_action_pressed(dir):
+			move(dir)
 
 func _process(_delta):
 	pass
 
+func move(dir: String) -> void:
+	ray.cast_to = DIRECTIONS[dir] * TILE_SIZE
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		self.position += DIRECTIONS[dir] * TILE_SIZE
+		self.position.x = clamp(self.position.x, TILE_SIZE/2.0, 1048)
+		self.position.y = clamp(self.position.y, TILE_SIZE/2.0, 620)
+
+
+func _on_Player_body_entered(body: Node) -> void:
+	if body.name == "Checkpoints":
+		print("Charging up, please stand by (this might take a wile...)")
+	elif body.name == "Target":
+		print("You completed this level successfully!")
+
 func decreas_moves_left():
 	moves_left -= 1
 	emit_signal("update_moves_left_counter", moves_left)
-	
