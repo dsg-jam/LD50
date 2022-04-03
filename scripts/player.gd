df688085631a27cc34425bd6dc636ec8453845f7
mineiwik <29404520+mineiwik@users.noreply.github.com>
2022-04-03 11:25:32 +0200

    Add player collision, checkpoints and target point

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,14 @@ extends Area2D
 
 var MAX_SPEED: int = 50
 var TILE_SIZE: int = 16
+var DIRECTIONS: Dictionary = {
+	"move_up": Vector2.UP,
+	"move_down": Vector2.DOWN,
+	"move_left": Vector2.LEFT,
+	"move_right": Vector2.RIGHT
+}
+
+onready var ray: RayCast2D = $RayCast2D
 
 func _ready():
 	self.position = self.position.snapped(Vector2.ONE * TILE_SIZE)
@@ -9,16 +17,24 @@ func _ready():
 
 
 func _unhandled_input(event):
-	if event.is_action_pressed("move_up"):
-		self.position += Vector2.UP * TILE_SIZE
-	elif event.is_action_pressed("move_down"):
-		self.position += Vector2.DOWN * TILE_SIZE
-	elif event.is_action_pressed("move_left"):
-		self.position += Vector2.LEFT * TILE_SIZE
-	elif event.is_action_pressed("move_right"):
-		self.position += Vector2.RIGHT * TILE_SIZE
-	self.position.x = clamp(self.position.x, TILE_SIZE/2.0, 1048)
-	self.position.y = clamp(self.position.y, TILE_SIZE/2.0, 620)
+	for dir in DIRECTIONS:
+		if event.is_action_pressed(dir):
+			move(dir)
 
 func _process(_delta):
 	pass
+
+func move(dir: String) -> void:
+	ray.cast_to = DIRECTIONS[dir] * TILE_SIZE
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		self.position += DIRECTIONS[dir] * TILE_SIZE
+		self.position.x = clamp(self.position.x, TILE_SIZE/2.0, 1048)
+		self.position.y = clamp(self.position.y, TILE_SIZE/2.0, 620)
+
+
+func _on_Player_body_entered(body: Node) -> void:
+	if body.name == "Checkpoints":
+		print("Charging up, please stand by (this might take a wile...)")
+	elif body.name == "Target":
+		print("You completed this level successfully!")
